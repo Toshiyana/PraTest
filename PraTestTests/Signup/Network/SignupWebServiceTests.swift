9e6cbca07222299cc48e578dd224d086a5159d21
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-04-18 14:17:20 +0900

    53: When empty URL provided return a specific error message

--- PraTestTests/Signup/Network/SignupWebServiceTests.swift
+++ PraTestTests/Signup/Network/SignupWebServiceTests.swift
@@ -63,5 +63,21 @@ class SignupWebServiceTests: XCTestCase {
         }
         self.wait(for: [expectation], timeout: 5)
     }
+    
+    func testSignupWebService_WhenEmptyURLStringProvided_ReturnsError() {
+        // Arrange
+        let expectation = self.expectation(description: "An empty request URL string expectation")
+        
+        sut = SignupWebService(urlString: "")
+        
+        // Act
+        sut.signup(withForm: signFormRequestModel) { (signupResponseModel, error) in
+            // Assert
+            XCTAssertEqual(error, SignupError.invalidRequestURLString, "The signup() method did not return an expected error for an invalidReqeustURLString error")
+            XCTAssertNil(signupResponseModel, "When an invalidRequestURLString take place, the response model must be nil")
+            expectation.fulfill()
+        }
+        self.wait(for: [expectation], timeout: 2)
+    }
 
 }
