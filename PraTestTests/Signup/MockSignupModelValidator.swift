0bbb88c93beac018e577c510ebcfc3c1bf650fb4
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-04-19 19:37:06 +0900

    62: Create Validator Protocol

--- PraTestTests/Signup/MockSignupModelValidator.swift
+++ PraTestTests/Signup/MockSignupModelValidator.swift
@@ -6,7 +6,30 @@
 //
 
 import Foundation
+@testable import PraTest
 
-class MockSignupModelValidator {
+class MockSignupModelValidator: SignupModelValidatorProtocol {
+
     var isFirstNameValidated = false
+    
+    func isFirstNameValid(firstName: String) -> Bool {
+        isFirstNameValidated = true
+        return isFirstNameValidated
+    }
+    
+    func isLastNameValid(lastName: String) -> Bool {
+        return true
+    }
+    
+    func isValidEmailFormat(email: String) -> Bool {
+        return true
+    }
+    
+    func isPasswordValid(password: String) -> Bool {
+        return true
+    }
+    
+    func doPasswordsMatch(password: String, repeatPassword: String) -> Bool {
+        return true
+    }
 }
