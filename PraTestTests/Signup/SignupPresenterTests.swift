2b1d29fd2a69a1aa74059cf2cfdecbfafdb85386
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-04-19 10:18:08 +0900

    58: Create Presenter unit test

---
+++ PraTestTests/Signup/SignupPresenterTests.swift
@@ -0,0 +1,28 @@
+//
+//  SignupPresenterTests.swift
+//  PraTestTests
+//
+//  Created by Toshiyana on 2022/04/19.
+//
+
+import XCTest
+
+class SignupPresenterTests: XCTestCase {
+
+    override func setUp() {
+        
+    }
+    
+    override func tearDown() {
+        
+    }
+    
+    func testSignupPresenter_WhenInformationProvided_WillValidateEachProperty() {
+        // Arrange
+        
+        // Act
+        
+        // Assert
+    }
+
+}
