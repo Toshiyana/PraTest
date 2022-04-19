b090ace5f715c2bb7191238cd8fa8cebaa326f71
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-04-19 19:28:42 +0900

    61: Create Initial MockSignupModelValidator class

---
+++ PraTestTests/Signup/MockSignupModelValidator.swift
@@ -0,0 +1,12 @@
+//
+//  MockSignupModelValidator.swift
+//  PraTestTests
+//
+//  Created by Toshiyana on 2022/04/19.
+//
+
+import Foundation
+
+class MockSignupModelValidator {
+    var isFirstNameValidated = false
+}
--- PraTestTests/Signup/SignupPresenterTests.swift
+++ PraTestTests/Signup/SignupPresenterTests.swift
@@ -25,13 +25,16 @@ class SignupPresenterTests: XCTestCase {
                                               email: "test@test.com",
                                               password:"12345678",
                                               repeatPassword:"12345678")
+        
+        let mockSignupModelValidator = MockSignupModelValidator()
+        
         let sut = SignupPresenter()
         
         // Act
         sut.processUserSignup(formModel: signupFormModel)
         
-        
         // Assert
+        XCTAssertTrue(mockSignupModelValidator.isFirstNameValidated)
     }
 
 }
