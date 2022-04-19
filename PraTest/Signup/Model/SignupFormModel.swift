d5e80aeffda299d76c998511edf06325f2cfe89f
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-04-19 10:23:41 +0900

    59: Create SignupFormModel Struct

---
+++ PraTest/Signup/Model/SignupFormModel.swift
@@ -0,0 +1,16 @@
+//
+//  SignupFormModel.swift
+//  PraTest
+//
+//  Created by Toshiyana on 2022/04/19.
+//
+
+import Foundation
+
+struct SignupFormModel {
+    let firstName: String
+    let lastName: String
+    let email: String
+    let password: String
+    let repeatPassword: String
+}
