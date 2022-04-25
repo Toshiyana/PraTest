2be03cc77cb70ccec909215c3531393207d357f3
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-04-25 13:03:20 +0900

    80. Test UITextFields are initially empty

--- PraTestTests/Signup/Controllers/SignupViewControllerTests.swift
+++ PraTestTests/Signup/Controllers/SignupViewControllerTests.swift
@@ -10,16 +10,27 @@ import XCTest
 
 class SignupViewControllerTests: XCTestCase {
 
+    var storyboard: UIStoryboard!
+    var sut: SignupViewController!
+    
     override func setUp() {
-        //
+        storyboard = UIStoryboard(name: "Main", bundle: nil)
+        sut = storyboard.instantiateViewController(identifier: "SignupViewController", creator: nil) as SignupViewController
+        sut.loadViewIfNeeded()
     }
     
     override func tearDown() {
-        //
+        storyboard = nil
+        sut = nil
     }
 
     func testSignupViewController_WhenCreated_HasRequiredTextFieldEmpty() {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let sut = storyboard.instantiateViewController(withIdentifier: "SignupViewController") as? SignupViewController
+        
+        
+        XCTAssertEqual(sut.firstNameTextField.text, "", "First name text field was not empty when the view controller initially loaded")
+        XCTAssertEqual(sut.lastNameTextField.text, "", "Last name text field was not empty when the view controller initially loaded")
+        XCTAssertEqual(sut.emailTextField.text, "", "Email text field was not empty when the view controller initially loaded")
+        XCTAssertEqual(sut.passwordTextField.text, "", "Password text field was not empty when the view controller initially loaded")
+        XCTAssertEqual(sut.repeatPasswordTextField.text, "", "Repeat password text field was not empty when the view controller initially loaded")
     }
 }
